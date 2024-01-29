385a9f5b86ad78f99e8a30411f58e943e1cae65e
Vivek Kumar <kumarvivek06071999@gmail.com>
2024-01-29 16:49:14 +0530

    HomePage import error fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Hero from '../components/hero/Hero'
-import TestPage from './testPage'
+
 
 const HomePage = () => {
     return (
